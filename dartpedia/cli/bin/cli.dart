f52716cb35e6aa65a4cee941608e490c3dd8b149
Subhadeep Ghoshal <subhadeep.ghoshal.work@gmail.com>
2026-02-13 18:53:28 +0530

    First commit

--- dartpedia/cli/bin/cli.dart
+++ dartpedia/cli/bin/cli.dart
@@ -1,5 +1,26 @@
-import 'package:cli/cli.dart' as cli;
+const version = '0.0.1'; // Add this line
 
 void main(List<String> arguments) {
-  print('Hello world: ${cli.calculate()}!');
+  if (arguments.isEmpty || arguments.first == 'help') {
+    printUsage();
+  } else if (arguments.first == 'version') {
+    print('Dartpedia CLI version $version');
+  } else if (arguments.first == 'search') {
+    final inputArgs = arguments.length > 1 ? arguments.sublist(1) : null;
+    searchWikipedia(inputArgs);
+  } else {
+    printUsage();
+  }
 }
+
+void searchWikipedia(List<String>? arguments) {
+  print('searchWikipedia received arguments: $arguments');
+}
+
+void printUsage() {
+  print(
+    "The following commands are valid: 'help', 'version', 'search <ARTICLE-TITLE>'",
+  );
+}
+
+
